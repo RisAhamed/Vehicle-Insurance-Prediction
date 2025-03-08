e0a25dc04da74d72466a7a8ee89e705b6aba5475
RisAhamed <riswanahamed38@gmail.com>
2025-03-08 16:23:10 +0530

    UI and pipeline created

---
+++ run.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+load_dotenv()
+from src.logger import logging
+from src.pipline.training_pipeline import TrainingPipeline
+from src.exception import CustomException
+import sys
+
+def main():
+    try:
+        training_pipeline = TrainingPipeline()
+        # data_ingestion_artifact = training_pipeline.start_data_ingestion()
+        # data_validation_artifact = training_pipeline.start_data_validation(data_ingestion_artifact)
+        training_pipeline.run_pipeline()
+    except Exception as e:
+        raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    main()
